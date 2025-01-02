03f85b6c67ccc8d4f584b21e308a7521d6f381b4
An <phamtrongan2710@gmail.com>
2025-01-03 00:59:03 +0700

    Drag and drop columns

--- src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
+++ src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
@@ -2,43 +2,52 @@ import Box from '@mui/material/Box'
 import Columns from './Column/Column'
 import Button from '@mui/material/Button'
 import AddBoxIcon from '@mui/icons-material/AddBox'
+import { SortableContext, horizontalListSortingStrategy } from '@dnd-kit/sortable'
 
 function ListColumns({ columns }) {
+  /**
+   * The <SortableContext> component requires that you pass it the sorted array of the
+   * unique identifiers associated to each sortable item via the items prop. This array
+   * should look like ["1", "2", "3"], not [{id: "1"}, {id: "2}, {id: "3}].
+   * If not, you can still drag and drop but there will be no animation.
+   */
   return (
-    <Box sx={{
-      bgcolor: 'inherit',
-      width: '100%',
-      height: '100%',
-      overflowX: 'auto',
-      overflowY: 'hidden',
-      display: 'flex',
-      '&::-webkit-scrollbar-track': { m: 2 }
-    }}>
-      {columns?.map(column => <Columns key={column._id} column={column} />)}
-
-      {/* Box add new column */}
+    <SortableContext items={columns?.map(c => c._id)} strategy={horizontalListSortingStrategy}>
       <Box sx={{
-        minWidth: '200px',
-        maxWidth: '200px',
-        mx: 2,
-        borderRadius: '6px',
-        height: 'fit-content',
-        bgcolor: '#ffffff3d'
+        bgcolor: 'inherit',
+        width: '100%',
+        height: '100%',
+        overflowX: 'auto',
+        overflowY: 'hidden',
+        display: 'flex',
+        '&::-webkit-scrollbar-track': { m: 2 }
       }}>
-        <Button
-          startIcon={<AddBoxIcon />}
-          sx={{
-            color: 'white',
-            width: '100%',
-            justifyContent: 'flex-start',
-            pl: 2.5,
-            py: 1
-          }}
-        >
-          Add new column
-        </Button>
+        {columns?.map(column => <Columns key={column._id} column={column} />)}
+
+        {/* Box add new column */}
+        <Box sx={{
+          minWidth: '200px',
+          maxWidth: '200px',
+          mx: 2,
+          borderRadius: '6px',
+          height: 'fit-content',
+          bgcolor: '#ffffff3d'
+        }}>
+          <Button
+            startIcon={<AddBoxIcon />}
+            sx={{
+              color: 'white',
+              width: '100%',
+              justifyContent: 'flex-start',
+              pl: 2.5,
+              py: 1
+            }}
+          >
+            Add new column
+          </Button>
+        </Box>
       </Box>
-    </Box>
+    </SortableContext>
   )
 }
 
